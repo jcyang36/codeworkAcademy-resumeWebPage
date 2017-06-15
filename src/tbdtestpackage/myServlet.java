db82820a856b42fd52fcdb4f20c09271ae933b14
Joyce Yang <jcyang@hmc.edu>
2017-06-15 09:55:13 -0400

    Added database for Education

--- src/tbdtestpackage/myServlet.java
+++ src/tbdtestpackage/myServlet.java
@@ -5,7 +5,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -42,16 +44,45 @@ public class myServlet extends HttpServlet {
 		String degree=request.getParameter("degree");
 		String field=request.getParameter("field");
 		String school=request.getParameter("school");
+		String YearGrad=request.getParameter("YearGrad");
 		String message="this is my message";
 		
 		
 		request.setAttribute("FirstLine", fName+" "+middle+" "+lName);
 		request.setAttribute("email", email);
 		request.setAttribute("education",degree+" in "+field);
-		request.setAttribute("classof",school);
+		request.setAttribute("classof",school+", "+YearGrad);
 		getServletContext().getRequestDispatcher("/NewJSPFile.jsp").forward(request,response);
 		
 		
+		try{Class.forName("com.mysql.jdbc.Driver");
+
+		con = DriverManager.getConnection("jdbc:mysql://localhost/WeekFourChallenge?"+ "user=root&password=password" );
+		
+		pstmt = con.prepareStatement("Insert into Education(degree,field,school,YearGrad) values (?,?,?,?) ");
+		pstmt.setString(1, degree);
+		pstmt.setString(2, field);
+		pstmt.setString(3, school);
+		pstmt.setString(4, YearGrad);
+  		pstmt.executeUpdate();
+		
+  		
+	
+
+		}catch(SQLException e){
+			e.printStackTrace();
+			
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				//rs.close();
+				pstmt.close();
+				con.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
 		
 		
 	}
